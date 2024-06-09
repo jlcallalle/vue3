c94d8bc9f23974a26bf7e61ac1cfb1b803341379
Jorge Callalle <vasslatam01@invex.com>
2024-06-09 17:17:19 -0500

    limpieza template

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,15 +1,5 @@
 <template>
   <div class="about">
-    <h1>This is an about page 123</h1>
+    <p>Página About</p>
   </div>
 </template>
-
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
-</style>
