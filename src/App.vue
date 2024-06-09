c890a7f900dbe94dde1457a2d666de78f15a6768
Jorge Callalle <vasslatam01@invex.com>
2024-06-09 18:38:09 -0500

    create page counterView

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,7 @@ import { RouterLink, RouterView } from 'vue-router'
     <div class="wrapper">
       <nav>
         <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/counter">Counter</RouterLink>
         <RouterLink to="/about">About</RouterLink>
       </nav>
     </div>
---
+++ src/shared/views/CounterView.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="about">
+    <p>Counter</p>
+  </div>
+</template>
