c55a5b361728fb554de32a2aa727e5a05a9a2741
Jorge Callalle <vasslatam01@invex.com>
2024-06-09 19:04:38 -0500

    fund coouter tradicional

--- src/shared/views/CounterView.vue
+++ src/shared/views/CounterView.vue
@@ -1,5 +1,24 @@
+<script lang="ts">
+import { defineComponent, ref } from "vue";
+
+export default defineComponent({
+  setup() {
+    const counter = ref(6);
+
+    const increase = () => {
+      counter.value++;
+    }
+    return {
+      counter,
+      increase,
+    }
+  }
+});
+</script>
+
 <template>
   <div class="about">
-    <p>Counter</p>
+    <p>Counter View: {{ counter }}</p>
+    <button @click="increase">+1</button>
   </div>
 </template>
