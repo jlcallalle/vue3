ad81f8dcb230067bb99317cabc9a5e1c2e2e659f
Jorge Callalle <vasslatam01@invex.com>
2024-06-09 21:23:06 -0500

    script setup vs setup mode

--- src/shared/views/CounterView.vue
+++ src/shared/views/CounterView.vue
@@ -1,33 +1,19 @@
-<script lang="ts">
-import { defineComponent, ref, computed } from "vue";
+<script setup lang="ts">
+import { ref } from "vue";
 
-export default defineComponent({
-  setup() {
+const message = ref('¡Hola, Mundo!');
+const counter = ref(0);
 
-    const message = ref('¡Hola, Mundo!');
-    const counter = ref(0);
-
-    const increase = () => {
+const increase = () => {
       counter.value++;
-    }
+}
 
-    const doubledCount = computed(() => counter.value * 2);
-
-    return {
-      counter,
-      increase,
-      message,
-      doubledCount
-    }
-  }
-});
 </script>
 
 <template>
   <div class="about">
     <p>Mensaje: {{ message }}</p>
     <p>Contador: {{ counter }}</p>
-    <p>Doble: {{ doubledCount }}</p>
     <button @click="increase">+1</button>
   </div>
 </template>
